b6cd664cea172e2bf564b95a3ffd79d90e164ec3
hanu1229 <han494127@gmail.com>
2024-11-21 16:08:57 +0900

    과제4 티켓 부분 수정중

--- day09/4_문자열.js
+++ day09/4_문자열.js
@@ -40,3 +40,48 @@ console.log("유재석\'강호동");
 console.log("유재석\\강호동");
 console.log("유재석\t강호동");
 console.log("=============");
+
+// [4] 문자열 연결, 문자열(`백틱)템플릿 --> 백틱 사이에 ${}을 작성하면 중괄호안에 js코드를 적을 수 있다.
+// JS에서 HTML을 작성시 JS코드와 함쳐서 작성할 수 있다.
+console.log('유재석' + '강호동');
+console.log('유재석' + 40);
+console.log('400' + '100');
+let age = 40;
+console.log("유재석" + age);
+console.log(`유재석${age}`);
+console.log(`평균 : ${100+50}`);
+
+let 직원명1 = "유재석";
+let 직원명2 = "강호동";
+let nameHTML = `<ol> <li>${직원명1}</li> <li>${직원명2}</li> </ol>`;
+document.write(nameHTML);
+
+// 예1 : 입력받은 데이터를 HTML로 출력하시오
+let result = prompt("이름을 입력하세요");
+let inputHTML = `<h3>입력받은 데이터 : ${result}</h3>`;
+document.write(inputHTML);
+
+// 예2 : 2명의 사원이름을 입력받아서 HTML(table)형식으로 출력하시오.
+/*
+
+*/
+let 직원이름 = [];
+직원이름[0] = prompt("이름을 입력하세요");
+직원이름[1] = prompt("이름을 입력하세요");
+let tableHTML = `
+                    <table> 
+                        <tr> 
+                            <th>번호</th> 
+                            <th>이름</th>
+                        </tr> 
+                        <tr> 
+                            <td>${직원이름.indexOf(직원이름[0])+1}</td> 
+                            <td>${직원이름[0]}</td> 
+                        </tr>
+                        <tr> 
+                            <td>${직원이름.indexOf(직원이름[1])+1}</td> 
+                            <td>${직원이름[1]}</td> 
+                        </tr> 
+                    </table>
+                    `;
+document.write(tableHTML);
